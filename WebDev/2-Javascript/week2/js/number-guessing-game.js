35c603fbd0ce0a9b93798b27aaa2f2cfc129edc1
Sai Balagi <balagisai92@gmail.com>
2023-10-28 10:32:23 -0700

    first time commit

---
+++ WebDev/2-Javascript/week2/js/number-guessing-game.js
@@ -0,0 +1,10 @@
+function runGame()
+{
+    const target = Math.floor(Math.random()*10)+1;
+    let guessString = '';
+    let guessNumber = 0;
+    let correct = false;
+    let numTries = 0;
+    do
+    )
+}
